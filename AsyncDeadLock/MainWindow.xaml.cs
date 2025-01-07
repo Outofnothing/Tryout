e6e07ad18a4621fece2389c816f1bde1316df3f1
zhou zikang <zikang.zhou@outlook.com>
2025-01-07 20:45:09 +0800

    Add project files.

---
+++ AsyncDeadLock/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace AsyncDeadLock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _button_Click(object sender, RoutedEventArgs e)
+        {
+            // Deadlock
+            Task task = WaitAsync();
+            task.Wait();
+        }
+
+        async Task WaitAsync()
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
