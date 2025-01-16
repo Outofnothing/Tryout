d3f83c7da908c26532430c0100ad067f05c64c2a
Zhou Zikang <zikang.zhou@outlook.com>
2025-01-16 17:52:21 +0800

    update property from background thread

---
+++ BehaviorOfAsync/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BehaviorOfAsync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            TryOpenFile();
+        }
+
+        private async void ReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            TryReadFile();
+        }
+
+        private async Task TryOpenFile()
+        {
+            using var file = File.OpenWrite("file.txt");
+            await Task.Delay(2000);
+            await file.WriteAsync(Encoding.UTF8.GetBytes("Hello, World!"));
+        }
+
+        private async Task TryReadFile()
+        {
+            using var file = File.OpenRead("file.txt");
+            var buffer = new byte[file.Length];
+            await file.ReadAsync(buffer, 0, buffer.Length);
+            MessageBox.Show(Encoding.UTF8.GetString(buffer));
+        }
+    }
+}
---
+++ UpdatePropertyFromBackgroundThread/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace UpdatePropertyFromBackgroundThread;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private bool _isBusy;
+
+    [ObservableProperty]
+    private string _message = "Waiting";
+
+    [RelayCommand]
+    private async Task SetIsBusyBackground()
+    {
+        await Task.Run(() => IsBusy = !IsBusy);
+    }
+
+    [RelayCommand]
+    private async Task SetMessageBackground()
+    {
+        await Task.Run(() => Message = "Hello, World!");
+    }
+
+    [RelayCommand]
+    private async Task OpenSubWindowInBackground()
+    {
+        await Task.Run(() =>
+        {
+            var subWindow = new SubWindow();
+            subWindow.Show();
+        });
+    }
+
+}
